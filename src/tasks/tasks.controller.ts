31f42beb3775ba5de10380fc43fa5ac1a355292e
leonkenzo1997 <truongtangquang1997@gmail.com>
2022-05-27 14:33:04 +0700

    api delete task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { CreateTaskDTO } from './dto/create-task.dto';
 import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
@@ -16,6 +16,11 @@ export class TasksController {
   getTaskById(@Param('id') id: string): Task {
     console.log('id', id);
     return this.tasksService.getTaskById(id);
+  }
+
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string): void {
+    this.tasksService.deleteTaskById(id);
   }
 
   @Post()
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -9,8 +9,13 @@ export class TasksService {
   getAllTasks(): Task[] {
     return this.tasks;
   }
+
   getTaskById(id: string): Task {
     return this.tasks.find((task) => task.id === id);
+  }
+
+  deleteTaskById(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
   createTask(createTaskDTO: CreateTaskDTO): Task {
